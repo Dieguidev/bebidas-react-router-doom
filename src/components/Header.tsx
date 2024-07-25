21ec430c13f1eb4fb7be11a079b240b096445847
Dieguidev <diegogaraycullas@gmail.com>
2024-07-25 07:16:24 -0500

    feat: Update Header component to conditionally render background image on tailwind.css

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,7 +7,7 @@ export const Header = () => {
   const isHome = useMemo(() => pathname === "/", [pathname]);
 
   return (
-    <header className="bg-slate-800">
+    <header className={isHome ? 'bg-header bg-center bg-cover':'bg-slate-800'}>
       <div className="mx-auto container px-5 py-16">
         <div className="flex justify-between items-center">
           <div>
