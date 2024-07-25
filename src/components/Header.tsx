a22ad89a7706fc61e32b4090daabe131b0a20e65
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 23:10:29 -0500

    feat: Add form for recipe search on home page
    The Header component in the Header.tsx file is updated to include a form for recipe search on the home page. This form allows users to enter a name or ingredient and select a category to search for recipes. The form is conditionally rendered only on the home page.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,10 @@
+import { useMemo } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 
 export const Header = () => {
+  const { pathname } = useLocation();
 
-  const location = useLocation();
-  console.log(location);
-
+  const isHome = useMemo(() => pathname === "/", [pathname]);
 
   return (
     <header className="bg-slate-800">
@@ -39,6 +39,48 @@ export const Header = () => {
             </NavLink>
           </nav>
         </div>
+        {isHome && (
+          <form className="md:w-1/2 2xl:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Nombre o Ingredientes
+              </label>
+
+              <input
+                type="text"
+                id="ingredient"
+                name="ingredient"
+                className="p-3 w-full rounded-lg focus:outline-none"
+                placeholder="Nombre o Ingrediente. Ej. Vodka, Tequila, Café"
+              />
+            </div>
+
+            <div className="space-y-4">
+              <label
+                htmlFor="ingredient"
+                className="block text-white uppercase font-extrabold text-lg"
+              >
+                Categoria
+              </label>
+
+              <select
+                id="category"
+                name="category"
+                className="p-3 w-full rounded-lg focus:outline-none"
+              >
+                <option value="">-- Seleccione --</option>
+              </select>
+            </div>
+            <input
+              type="submit"
+              value="Buscar Recetas"
+              className="cursor-pointer bg-orange-800 hover:bg-orange-900 text-white font-extrabold w-full p-2 rounded-lg uppercase"
+            />
+          </form>
+        )}
       </div>
     </header>
   );
