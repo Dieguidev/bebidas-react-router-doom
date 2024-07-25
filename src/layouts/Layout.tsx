6341a1c716d6cbead97a8a1e61a5b14b2044d87e
Dieguidev <diegogaraycullas@gmail.com>
2024-07-25 18:42:59 -0500

    feat: Se actualiza la pagina de favoritos para que muestre en tarjetas las bebidas favoritas

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,8 +1,18 @@
 import { Outlet } from "react-router-dom";
 import { Header } from "../components/Header";
 import Modal from "../components/Modal";
+import { useEffect } from "react";
+import { useAppStore } from "../stores/useAppStore";
 
 export const Layout = () => {
+
+  const {loadFromStorage} = useAppStore()
+
+  useEffect(() => {
+    loadFromStorage()
+  }, [])
+
+
   return (
     <>
       <Header />
--- src/views/FavoritesPage.tsx
+++ src/views/FavoritesPage.tsx
@@ -1,10 +1,29 @@
+import { useMemo } from "react";
+import { DrinkCard } from "../components/DrinkCard";
+import { useAppStore } from "../stores/useAppStore"
 
 
 
 export const FavoritesPage = () => {
+
+  const {favorites} = useAppStore();
+  const hasFavorites = useMemo(() => favorites.length, [favorites])
+
   return (
     <>
-      <h1>Favoritos</h1>
+      <h1 className="text-6xl font font-extrabold">Favoritos</h1>
+      {hasFavorites ? (
+      <div className="grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 my-10 gap-10">
+        {
+          favorites.map(favorite =>(
+            <DrinkCard key={favorite.idDrink} drink={favorite} />
+          ))
+        }
+      </div>
+
+      ): (
+        <p className="my-10 text-center text-2xl">Los favoritos se mostraran aquí</p>
+      )}
     </>
   )
 }
