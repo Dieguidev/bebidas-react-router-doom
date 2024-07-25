b91dcf15719e0e8362a2765883ef1eb136569b58
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 22:36:58 -0500

    useLocation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,11 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 
 export const Header = () => {
+
+  const location = useLocation();
+  console.log(location);
+
+
   return (
     <header className="bg-slate-800">
       <div className="mx-auto container px-5 py-16">
