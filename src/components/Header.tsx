c9399cfd914f273fb489d286315e5f65c473be97
Dieguidev <diegogaraycullas@gmail.com>
2024-07-24 22:33:35 -0500

    feat: Update Header component to use NavLink instead of Link
    The Header component in the Header.tsx file is modified to use NavLink instead of Link for navigation. This change improves the user experience by applying a different styling to the active link. The code changes also include updating the class names and adding conditional rendering logic based on the active state of the NavLink.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export const Header = () => {
   return (
@@ -12,12 +12,26 @@ export const Header = () => {
           </div>
 
           <nav className="flex gap-4">
-            <Link to="/" className="text-white uppercase font-bold">
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
               Inicio
-            </Link>
-            <Link to="/favoritos" className="text-white uppercase font-bold">
+            </NavLink>
+            <NavLink
+              to="/favoritos"
+              className={({ isActive }) =>
+                isActive
+                  ? "text-orange-500 uppercase font-bold"
+                  : "text-white uppercase font-bold"
+              }
+            >
               Favoritos
-            </Link>
+            </NavLink>
           </nav>
         </div>
       </div>
