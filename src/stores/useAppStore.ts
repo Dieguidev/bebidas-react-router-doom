15a2ff3c82994ca58f0770f04559a802ef0a6494
Dieguidev <diegogaraycullas@gmail.com>
2024-07-25 08:09:29 -0500

    feat: Update useAppStore to include devtools middleware
    The `useAppStore` function in `useAppStore.ts` is updated to include the `devtools` middleware from the `zustand` library. This middleware enhances the debugging capabilities of the application by providing a visual representation of the store's state and actions in the browser's developer tools.

--- src/stores/useAppStore.ts
+++ src/stores/useAppStore.ts
@@ -1,7 +1,8 @@
 import { create } from "zustand";
 import { createRecipesSlice, RecipesSliceType } from "./recipeSlice";
+import { devtools } from "zustand/middleware";
 
 
-export const useAppStore = create<RecipesSliceType>((...a) => ({
+export const useAppStore = create<RecipesSliceType>()(devtools((...a) => ({
   ...createRecipesSlice(...a),
-}))
+})))
