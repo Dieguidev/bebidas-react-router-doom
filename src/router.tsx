5864042260e11df0d2277f7bd2c6ed1fafb3a658
Dieguidev <diegogaraycullas@gmail.com>
2024-07-25 20:41:46 -0500

    feat: Implementar lazy loading en la página de favoritos para mejorar el rendimiento de carga
    Se implementa el lazy loading en la página de favoritos utilizando la función Suspense y lazy de React. Esto permite cargar la página de favoritos de forma diferida, mejorando el rendimiento de carga de la aplicación. Además, se muestra un mensaje de carga mientras se carga la página. Estos cambios mejoran la experiencia del usuario al reducir el tiempo de carga inicial de la página de favoritos.

--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,10 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { IndexPage } from "./views/IndexPage";
-import { FavoritesPage } from "./views/FavoritesPage";
+// import { FavoritesPage } from './views/FavoritesPage';
 import { Layout } from "./layouts/Layout";
+import { lazy, Suspense } from "react";
+
+const FavoritesPage = lazy(() => import('./views/FavoritesPage').then(module => ({ default: module.FavoritesPage })));
 
 export const AppRouter = () => {
   return (
@@ -9,7 +12,11 @@ export const AppRouter = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<IndexPage />} />
-          <Route path="/favoritos" element={<FavoritesPage />} />
+          <Route path="/favoritos" element={
+            <Suspense fallback='Cargando...'>
+              <FavoritesPage />
+            </Suspense>
+          } />
         </Route>
       </Routes>
     </BrowserRouter>
